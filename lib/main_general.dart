82f4eabc000e3e9ccba1ed5c3ef86c9f99e8f636
Valdo Souza <devops@setes.com.br>
2024-03-05 16:16:52 -0300

    Ajustes firebse Analytics

--- lib/main_general.dart
+++ lib/main_general.dart
@@ -1,11 +1,12 @@
 import 'dart:ui';
+import 'package:appweb/app/core/shared/helpers/analytics_engine.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 void init() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  //await AnalyticsEngine.init();
+  await AnalyticsEngine.init();
 
   FlutterError.onError = (errorDetails) {
     // If you wish to record a "non-fatal" exception, please use `FirebaseCrashlytics.instance.recordFlutterError` instead
